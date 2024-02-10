afebcb300b670a476f5183d86a4b2e194651fb93
trana261003 <130343068+trana261003@users.noreply.github.com>
2024-02-10 13:06:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let move_speed = 3, grativy = 0.5;
 let bird = document.querySelector('.bird');
 let img = document.getElementById('bird-1');
-let sound_point = new Audio('sounds effect/point.mp3');
-let sound_die = new Audio('sounds effect/die.mp3');
+let sound_point = new Audio('point.mp3');
+let sound_die = new Audio('die.mp3');
 
 // getting bird element properties
 let bird_props = bird.getBoundingClientRect();
@@ -73,14 +73,14 @@ function play(){
         bird_dy = bird_dy + grativy;
         document.addEventListener('keydown', (e) => {
             if(e.key == 'ArrowUp' || e.key == ' '){
-                img.src = 'images/Bird-2.png';
+                img.src = 'Bird-2.png';
                 bird_dy = -7.6;
             }
         });
 
         document.addEventListener('keyup', (e) => {
             if(e.key == 'ArrowUp' || e.key == ' '){
-                img.src = 'images/Bird.png';
+                img.src = 'Bird.png';
             }
         });
 
